be63843f945e83fff0b03a2db491d601753a6b80
Adam Brannon <adam1320@rocketmail.com>
2022-04-08 13:25:52 -0400

    Completed JS exercise1

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Adam";
+
+let lastName = "Brannon";
+
+var age = 46;
 
 // Exercise 2
+let fullName = firstName + ' ' + lastName;
+let templateFullName = `${firstName} ${lastName}`;
 
-// Exercise 3
+console.log (fullName);
+console.log (firstName + ' ' + lastName);
+console.log (templateFullName);
+//BONUS
+console.log(`Hello, my name is ${firstName} ${lastName}, and I am ${age} years old.`);
+console.log(`Hello, my name is ${fullName}, and I am ${age} years old. You killed my Father. Prepare to die.`)
+
+//Exercise 3
+
+let city = "Urbana, Ohio";
+let pasTime = "bowling";
+let myStory = "Hello, my name is "+fullName+", and I am "+age+" years old. I am from "+city+".\nIn my spare time, I enjoy a number of hobbies, I am particularly fond of "+pasTime+".";
+let templateMyStory = `Hello, my name is ${fullName}, and I am ${age} years old.
+I am from ${city}.
+I have a number of hobbies, one of my favorites is ${pasTime}.`;
+console.log(myStory);
+console.log(templateMyStory);
+
